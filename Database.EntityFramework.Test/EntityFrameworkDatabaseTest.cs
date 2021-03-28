57c09681f8d3de8ddcd8eab2832a38722e5f020b
Artyom Sovetnikov <elringus@gmail.com>
2021-03-28 22:13:05 +0300

    add ef

---
+++ Database.EntityFramework.Test/EntityFrameworkDatabaseTest.cs
@@ -0,0 +1,18 @@
+﻿using Database.Test;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.EntityFramework.Test
+{
+    public class EntityFrameworkDatabaseTest : DatabaseTest
+    {
+        public EntityFrameworkDatabaseTest ()
+            : base(new EntityFrameworkDatabase(CreateOptions())) { }
+
+        private static DbContextOptions CreateOptions ()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseInMemoryDatabase("MockDatabase");
+            return optionsBuilder.Options;
+        }
+    }
+}
---
+++ Database.EntityFramework/EntityFrameworkDatabase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.EntityFramework
+{
+    public class EntityFrameworkDatabase : IDatabase
+    {
+        private readonly DbContextOptions contextOptions;
+
+        public EntityFrameworkDatabase (DbContextOptions contextOptions)
+        {
+            this.contextOptions = contextOptions;
+        }
+
+        public IReference<T> Add<T> (T record) where T : notnull
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T> (IReference<T> reference)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<T> (IReference<T> reference, T record) where T : notnull
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove<T> (IReference<T> reference)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<(IReference<T> Reference, T Record)> Query<T> ()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITransaction Transact (Action action)
+        {
+            throw new NotImplementedException();
+        }
+
+        private DbContext CreateContext () => new(contextOptions);
+    }
+}
